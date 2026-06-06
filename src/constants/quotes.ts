3fcb8ae647a8ba641da909f4203cdc079d34eb1a
Lasse Stilvang <lasse.stilvang@gmail.com>
2026-06-06 23:23:00 +0200

    feat(constants): add quotes constant for empty state

---
+++ src/constants/quotes.ts
@@ -0,0 +1,7 @@
+export const QUOTES = [
+  "The way to get started is to quit talking and begin doing.",
+  "Don't let yesterday take up too much of today.",
+  "Action is the foundational key to all success.",
+  "Your mind is for having ideas, not holding them.",
+  "Focus on being productive instead of busy.",
+];
